ac03468ca876ccce305dd824fa0d50965e383703
Rodrigo <rodrigovieiranunes23@outlook.com>
2021-05-24 20:06:20 -0300

    finished  interface login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import {View, StyleSheet, Image, TextInput} from 'react-native';
-const imageStarteds = require('../../assets/starteds.jpg');
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../screens/RootStackPrams';
+type authScreenProp = StackNavigationProp<RootStackParamList, 'Login'>;
+import {Input} from 'react-native-elements';
+const logoUser = require('../../assets/logo.png');
+export var width = Dimensions.get('window').width;
 
-export default class Login extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View>
-          <TextInput value="texto" />
-        </View>
-        <View style={styles.image}>
-          <Image style={styles.images} source={imageStarteds} />
-        </View>
+const Login = () => {
+  const navigation = useNavigation<authScreenProp>();
+  return (
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <Image style={styles.images} source={logoUser} />
       </View>
-    );
-  }
-}
+
+      <View style={styles.header}>
+        <Text>Email</Text>
+        <Input placeholder="EMAIL" />
+      </View>
+      <View style={styles.header}>
+        <Text>Password</Text>
+        <Input placeholder="Password" />
+      </View>
+      <TouchableOpacity
+        style={styles.btnLogin}
+        onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.btntextLogin}>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     color: 'white',
-    justifyContent: 'center',
     alignItems: 'center',
     paddingLeft: 25,
     paddingRight: 25,
@@ -29,7 +51,28 @@ const styles = StyleSheet.create({
   },
   image: {},
   images: {
-    width: 236,
-    height: 166,
+    width: 130,
+    height: 130,
+    marginTop: 40,
+  },
+  btnLogin: {
+    width: width - 50,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: '#d26acc',
+  },
+  btntextLogin: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#d26acc',
+  },
+
+  header: {
+    width: width - 50,
   },
 });
+
+export default Login;
